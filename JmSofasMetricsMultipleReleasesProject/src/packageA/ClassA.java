3f3155760127ed7e10fc02b995bcbcd14714956f
Nendor <marc.a.weber@gmx.ch>
2013-06-13 17:21:24 +0200

    Added more complexity to privateVoidMethod1ClassA().

--- JmSofasMetricsMultipleReleasesProject/src/packageA/ClassA.java
+++ JmSofasMetricsMultipleReleasesProject/src/packageA/ClassA.java
@@ -2,14 +2,41 @@ package packageA;
 
 public class ClassA {
 
-	public void publicVoidMethod1ClassA(){
-		
+	public void publicVoidMethod1ClassA() {
+
 	}
-	public void publicVoidMethod2ClassA(){
+
+	public void publicVoidMethod2ClassA() {
 		privateVoidMethod1ClassA();
 	}
-	
-	private void privateVoidMethod1ClassA(){
+
+	private void privateVoidMethod1ClassA() {
 		System.out.println("");
+		if (true) {
+			if (true) {
+				if (true) {
+					if (true) {
+						if (true) {
+							if (true) {
+								if (true) {
+									if (true) {
+									}
+								}
+								if (true) {
+									if (true) {
+										if (true) {
+											if (true) {
+											}
+										}
+									}
+								}
+								if (true) {
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 }
